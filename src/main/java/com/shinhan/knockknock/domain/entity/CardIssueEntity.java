88f1728ddcc77371281a6dbbb1b8fd1a308a94d5
YangxGeon <https://github.com/YangxGeon>
2024-08-13 14:32:26 +0900

    [Feat]: 카드 발급 비동기 처리

--- src/main/java/com/shinhan/knockknock/domain/entity/CardIssueEntity.java
+++ src/main/java/com/shinhan/knockknock/domain/entity/CardIssueEntity.java
@@ -1,6 +1,7 @@
 package com.shinhan.knockknock.domain.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,47 +24,59 @@ public class CardIssueEntity {
     @Column(name= "cardissue_no")
     private Long cardIssueNo;
 
-    @Column(name= "cardissue_residentno", length = 50, nullable= false)
+    @Column(name= "cardissue_residentno", length = 50)
+    @NotNull
     private String cardIssueResidentNo;
 
-    @Column(name= "cardissue_ename", length = 50, nullable= false)
+    @Column(name= "cardissue_ename", length = 50)
+    @NotNull
     private String cardIssueEname;
 
     @Column(name= "cardissue_email", length = 50)
     private String cardIssueEmail;
 
-    @Column(name= "cardissue_bank", length = 20, nullable= false)
+    @Column(name= "cardissue_bank", length = 20)
+    @NotNull
     private String cardIssueBank;
 
-    @Column(name= "cardissue_account", length = 50, nullable= false)
+    @Column(name= "cardissue_account", length = 50)
+    @NotNull
     private String cardIssueAccount;
 
-    @Column(name= "cardissue_isagree", nullable= false)
+    @Column(name= "cardissue_isagree")
+    @NotNull
     private boolean cardIssueIsAgree;
 
-    @Column(name= "cardissue_income", nullable= false)
+    @Column(name= "cardissue_income")
+    @NotNull
     private int cardIssueIncome;
 
-    @Column(name= "cardissue_credit", nullable= false)
+    @Column(name= "cardissue_credit")
+    @NotNull
     private int cardIssueCredit;
 
-    @Column(name= "cardissue_amountdate", nullable= false)
+    @Column(name= "cardissue_amountdate")
+    @NotNull
     private Date cardIssueAmountDate;
 
-    @Column(name= "cardissue_source", nullable= false)
+    @Column(name= "cardissue_source")
+    @NotNull
     private String cardIssueSource;
 
-    @Column(name= "cardissue_ishighrisk", nullable= false)
+    @Column(name= "cardissue_ishighrisk")
+    @NotNull
     private boolean cardIssueIsHighrisk;
 
-    @Column(name= "cardissue_purpose", nullable= false)
+    @Column(name= "cardissue_purpose")
+    @NotNull
     private String cardIssuePurpose;
 
     @Column(name= "cardissue_issuedate")
     @CreationTimestamp
     private Timestamp cardIssueIssueDate;
 
-    @Column(name= "user_no", nullable= false)
+    @Column(name= "user_no")
+    @NotNull
     private Long userNo;
 
 }
