01336d1adf1956f5c967267f83416238f8a6af34
unknown <isakin.tyrfing@gmail.com>
2024-08-19 10:20:58 +0900

    [Refactor]: card history 테이블 명 rollback

--- src/main/java/com/shinhan/knockknock/domain/entity/CardHistoryEntity.java
+++ src/main/java/com/shinhan/knockknock/domain/entity/CardHistoryEntity.java
@@ -14,7 +14,7 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "cardhistory_tb_1")
+@Table(name = "cardhistory_tb")
 public class CardHistoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
