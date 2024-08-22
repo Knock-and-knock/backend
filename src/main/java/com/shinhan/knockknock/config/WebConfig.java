b8e034705e80449ccf4dd2f765594cecce20253b
unknown <isakin.tyrfing@gmail.com>
2024-08-22 15:10:53 +0900

    [Feat]: CORS 설정 추가

---
+++ src/main/java/com/shinhan/knockknock/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.shinhan.knockknock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("*") // 모든 출처 허용
+                .allowedMethods("*") // 모든 HTTP 메서드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true) // 자격 증명 허용
+                .maxAge(3600); // CORS 정책의 캐시 지속 시간 설정
+    }
+}
