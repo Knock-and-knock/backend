4096836ef003e6f4c56c636171df903e176815e9
DaeHyun2 <hh5576@naver.com>
2024-08-22 12:02:13 +0900

    [Refactor]: code 수정

--- src/test/java/com/shinhan/knockknock/controller/CardHistoryControllerTest.java
+++ src/test/java/com/shinhan/knockknock/controller/CardHistoryControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.NoSuchElementException;
@@ -21,7 +22,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@ActiveProfiles("test")
 @WebMvcTest(CardHistoryController.class)
 class CardHistoryControllerTest {
 
