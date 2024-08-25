0a80558de2e3279cd38c782ada16e7c2959d9798
DaeHyun2 <hh5576@naver.com>
2024-08-25 17:00:56 +0900

    [Refactor]: 카드 조회 코드 수정

--- src/main/java/com/shinhan/knockknock/repository/CardHistoryRepository.java
+++ src/main/java/com/shinhan/knockknock/repository/CardHistoryRepository.java
@@ -3,6 +3,8 @@ package com.shinhan.knockknock.repository;
 import com.shinhan.knockknock.domain.entity.CardHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardHistoryRepository extends JpaRepository<CardHistoryEntity, Long> {
+import java.util.List;
 
+public interface CardHistoryRepository extends JpaRepository<CardHistoryEntity, Long> {
+    List<CardHistoryEntity> findByCard_CardId(Long cardId);
 }
