458536ad1322b19a39eafedfd9e84717f123bcba
wjdals898 <jungmin898@naver.com>
2024-08-31 22:56:01 +0900

    [Feat]: user & card 기본 sql 데이터 추가

---
+++ src/main/resources/sql/card.sql
@@ -0,0 +1,19 @@
+INSERT INTO card_tb (
+    card_no,
+    card_cvc,
+    card_ename,
+    card_password,
+    card_bank,
+    card_account,
+    card_amountdate,
+    card_expiredate,
+    cardissue_no,
+    user_no,
+    card_isfamily,
+    card_address
+) VALUES
+      ('1234567890123456', '123', 'John Doe', 'password123', 'Shinhan Bank', '110-123-456789', '12', '2025-12-31', 1001, 1, false, '123 Main St, Seoul, Korea'),
+      ('6543210987654321', '456', 'Jane Smith', 'securePass456', 'Kookmin Bank', '220-987-654321', '15', '2026-06-30', 1002, 2, true, '456 Elm St, Busan, Korea'),
+      ('1111222233334444', '789', 'Alice Johnson', 'pass789!', 'Hana Bank', '330-123-987654', '18', '2027-03-31', 1003, 3, false, '789 Oak St, Incheon, Korea'),
+      ('4444333322221111', '101', 'Bob Lee', 'qwerty101', 'Woori Bank', '440-456-123456', '25', '2028-09-30', 1004, 4, true, '321 Pine St, Daegu, Korea'),
+      ('9999888877776666', '202', 'Charlie Brown', 'charlie202!', 'NH Nonghyup Bank', '550-789-321654', '05', '2024-05-31', 1005, 5, false, '654 Cedar St, Ulsan, Korea');
---
+++ src/main/resources/sql/user.sql
@@ -0,0 +1,24 @@
+INSERT INTO user_tb
+(user_birth, user_gender, user_height, user_iswithdraw, user_joindate, user_weight, user_address, user_disease, user_id, user_name, user_password, user_phone, user_simplepassword, user_type, user_biopassword)
+VALUES(NULL, 0, 0, false, '2024-08-22', 0, NULL, NULL, 'protector03', '보호자03', '$2a$10$OQwsovTdqOz1dWApur5wT.372wTtTNPg7XUozVoLwUC0FT2sZTfRq', '01099998888', '$2a$10$z9curAld2M7l48r1u2pvb.q.lBYxq.0Oz5zIThI2U064dH8a8XXlO', 'PROTECTOR', NULL);
+INSERT INTO user_tb
+(user_birth, user_gender, user_height, user_iswithdraw, user_joindate, user_weight, user_address, user_disease, user_id, user_name, user_password, user_phone, user_simplepassword, user_type, user_biopassword)
+VALUES(NULL, 0, 0, false, '2024-08-22', 0, NULL, NULL, 'protector01', '보호자01', '$2a$10$GVoFCI0UVrsL9pTcvm/7PeGOsdX4Z68n2VeWJ.eUhbgQptGkQqiuK', '01012345678', '$2a$10$ptcSFYwKLz9GrFcpfD8duOcWBHAcOIl50/yZJ0MAPSpnNpyvRXELe', 'PROTECTOR', NULL);
+INSERT INTO user_tb
+(user_birth, user_gender, user_height, user_iswithdraw, user_joindate, user_weight, user_address, user_disease, user_id, user_name, user_password, user_phone, user_simplepassword, user_type, user_biopassword)
+VALUES(NULL, 0, 0, false, '2024-08-22', 0, NULL, NULL, 'tmdrjs1', '보호자02', '$2a$10$2evPeNllgyhKjvM5o4p1/.Bzoy.66bf/Vs1cHzZMkuNLEQ5XvP9QK', '01012345677', '$2a$10$WW7jxLZat7aiVBGNho5WuexEHV7y55/bfVi7dwIGNZDUEYiChhRQO', 'PROTECTOR', NULL);
+INSERT INTO user_tb
+(user_birth, user_gender, user_height, user_iswithdraw, user_joindate, user_weight, user_address, user_disease, user_id, user_name, user_password, user_phone, user_simplepassword, user_type, user_biopassword)
+VALUES('1968-03-08', 1, 175, false, '2024-08-22', 76, '서울시 마포구 월드컵대로/101동203호', '고혈압', 'protege02', '피보호자02', '$2a$10$iqnkSFGBHCKFn8dJbfEgFeerPAfpFJLLobXP52s6f/cNo4PqV4h5u', '01012345677', '$2a$10$aAZkTEFQNQ9HNJ0Fyv8vwuDdpZRJ1TfdDFpgwFG4Xz7SkBsANrFTu', 'PROTEGE', NULL);
+INSERT INTO user_tb
+(user_birth, user_gender, user_height, user_iswithdraw, user_joindate, user_weight, user_address, user_disease, user_id, user_name, user_password, user_phone, user_simplepassword, user_type, user_biopassword)
+VALUES('1954-08-24', 2, 168, false, '2024-08-22', 66, '서울특별시 마포구 월드컵북로4길 77/1층 2강의실', '당뇨, 고혈압', 'test', 'test', '$2a$10$Af1dMKPPwkTFkVceKAyj8.EzcVZdY7mzS2b394cUUXcufnq2CAHVO', '01087654321', '$2a$10$uSfIBekuRZA0xr8ihTGDWuKDPvVrUj5wiYQwQx7nVavzE82ygMHCi', 'PROTEGE', NULL);
+INSERT INTO user_tb
+(user_birth, user_gender, user_height, user_iswithdraw, user_joindate, user_weight, user_address, user_disease, user_id, user_name, user_password, user_phone, user_simplepassword, user_type, user_biopassword)
+VALUES(NULL, 0, 0, false, '2024-08-27', 0, NULL, NULL, 'protector04', '보호자03', '$2a$10$ByQ0x3s.LmxhzUhKhM4w2OusCWZO8YnlUq2jvQEBuTJTTnM99wm/C', '01099991234', '$2a$10$0qkWr.4irLO2I6GsbM/vRe3VGpqtfLnB6BBCJd/4BGgPv.dsEX1Aa', 'PROTECTOR', '$2a$10$tLVv/jQjCrPzq71kvGKUUu7OWZcgkBBzVCc5qXHOW7.ZRB.k8uVoi');
+INSERT INTO user_tb
+(user_birth, user_gender, user_height, user_iswithdraw, user_joindate, user_weight, user_address, user_disease, user_id, user_name, user_password, user_phone, user_simplepassword, user_type, user_biopassword)
+VALUES('2024-08-29', 2, 161, false, '2024-08-22', 54, '서울시 마포구 월드컵대로', '고혈압', 'test01', '테스트01', '$2a$10$vs.Xe1huTYyfonCodNSRbeztn1o93yhm05F3PYrj0gvagNcuLmzV6', '01099998888', '$2a$10$lW9Dq3LJmgigM6OJjx8li.xaG/IW4Sai65FuGuGLemWTA/n1YstZa', 'PROTEGE', NULL);
+INSERT INTO user_tb
+(user_birth, user_gender, user_height, user_iswithdraw, user_joindate, user_weight, user_address, user_disease, user_id, user_name, user_password, user_phone, user_simplepassword, user_type, user_biopassword)
+VALUES('2024-08-30', 2, 161, false, '2024-08-22', 54, NULL, '고혈압', 'protege01', '피보호자01', '$2a$10$roeqDhDsO.e/1kl88itsI.5pw/icyxOwNEx7z3.TYEyA/tqVWMU9O', '01012345678', '$2a$10$PfxEDhIIlvcoosaYmIzWkehANIf7yiumN1XyqvvWVUEE2uO0zV/WO', 'PROTEGE', NULL);
