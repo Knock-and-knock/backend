0352ef708dcdfae97befcb5c4c4b2e6aac657336
YangxGeon <https://github.com/YangxGeon>
2024-09-01 23:54:36 +0900

    [Feat] sql

---
+++ src/main/resources/sql/cardissue.sql
@@ -0,0 +1,17 @@
+INSERT INTO cardissue_tb (
+    cardissue_isagree,
+    cardissue_isfamily,
+    cardissue_issuedate,
+    user_no,
+    cardissue_bank,
+    cardissue_account,
+    cardissue_credit,
+    cardissue_email,
+    cardissue_income,
+    cardissue_address,
+    cardissue_amountdate,
+    cardissue_ishighrisk,
+    cardissue_purpose,
+    cardissue_source
+) VALUES ( true,false,'2024-08-30 23:26:06.586', 9,'kakao','1234-123121-22',0,'bohozanim@naver.com','1억','juso', '2024-08-12','highRisk','use','working'),
+         (true,true,'2024-09-01 23:26:06.586',9, 'kakao','1234-123121-22',0,'pibohozanim@naver.com','1원','juso','2024-08-12','highRisk','use','working');
