7f5e46378a7da9dc76d91501a6da8bb1a3961e51
wjdals898 <jungmin898@naver.com>
2024-09-01 23:52:05 +0900

    [Feat]: match 데이터 추가

---
+++ src/main/resources/sql/match.sql
@@ -0,0 +1,6 @@
+INSERT INTO match_tb (user_protectorno, user_protegeno, match_protectorname, match_protegename, match_status)
+VALUES (9, 10, '나나', '너너', 'ACCEPT');
+INSERT INTO match_tb (user_protectorno, user_protegeno, match_protectorname, match_protegename, match_status)
+VALUES (1, 5, '동희', '성진', 'ACCEPT');
+INSERT INTO match_tb (user_protectorno, user_protegeno, match_protectorname, match_protegename, match_status)
+VALUES (2, 6, '딸', '엄마', 'ACCEPT');
