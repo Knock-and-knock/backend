f616351fa0fa0a106581f6172881c804d553dd84
DaeHyun2 <hh5576@naver.com>
2024-09-01 17:26:15 +0900

    [Feat]: FDS 알림기능

--- src/main/java/com/shinhan/knockknock/repository/CardRepository.java
+++ src/main/java/com/shinhan/knockknock/repository/CardRepository.java
@@ -19,4 +19,7 @@ public interface CardRepository extends JpaRepository<CardEntity, Long> {
     // 보호자 번호와 가족 카드 여부로 카드 조회
     @Query("SELECT c FROM CardEntity c WHERE c.userNo = :userNo AND c.cardIsfamily = true")
     List<CardEntity> findFamilyCardsByUserNo(@Param("userNo") Long userNo);
+
+    @Query("SELECT c.userNo FROM CardEntity c WHERE c.cardId = :cardId")
+    Long findUserNoByCardId(@Param("cardId") Long cardId);
 }
